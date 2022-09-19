091b130fb06aa0cf60b613968c470f2f5347499e
Peraccjm <peraccjm@mail.uc.edu>
2022-09-18 22:00:33 -0400

    powershell

---
+++ powershell/sysinfo_peraccjm.ps1
@@ -0,0 +1,26 @@
+﻿function getIP{
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+    }
+function getUSER{
+    whoami
+}
+function getHOST{
+    hostname
+    }
+function getPSMV{
+    $Host.Version.Major
+    }
+Function getDATE{
+    Get-Date -format "dddd MM/dd/yyyy"
+    }
+
+
+$IP = getIP
+$HOSTNAME = getHOST
+$PSMV = getPSMV
+$date = getDATE
+$user = getUSER
+
+echo("This machine's IP is $IP. User is $user. Hostname is $HOSTNAME. PowerShell Version $PSMV. Today's Date is $date.") | out-file -FilePath C:\IT-3080c-Scripts\powershell\sysinfo.txt
+
+
